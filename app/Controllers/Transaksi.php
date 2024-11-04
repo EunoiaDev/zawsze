d3517cec73e42ed1c3ff4b91143d9b813e5d2095
Eunoia <eunoiadevelopteam@gmail.com>
2024-11-04 12:03:59 +0700

    penggunaan revisi oleh chat gpt thdp file Transaksi.PHP yang error tidak diketahui

--- app/Controllers/Transaksi.php
+++ app/Controllers/Transaksi.php
@@ -8,86 +8,14 @@ use CodeIgniter\RESTful\ResourceController;
 
 class Transaksi extends ResourceController
 {
-    function __construct()
+    protected $db;
+    protected $objTransaksi;
+
+    public function __construct()
     {
-        $this->db = \config\Database::connect();
+        $this->db = \Config\Database::connect();
         $this->objTransaksi = new ModelTransaksi();
     }
-    /**
-     * Return an array of resource objects, themselves in array format.
-     *
-     * @return ResponseInterface
-     */
-    public function index()
-    {
-        return view('transaksi/index');
-    }
 
-    /**
-     * Return the properties of a resource object.
-     *
-     * @param int|string|null $id
-     *
-     * @return ResponseInterface
-     */
-    public function show($id = null)
-    {
-        //
-    }
-
-    /**
-     * Return a new resource object, with default properties.
-     *
-     * @return ResponseInterface
-     */
-    public function new()
-    {
-        //
-    }
-
-    /**
-     * Create a new resource object, from "posted" parameters.
-     *
-     * @return ResponseInterface
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Return the editable properties of a resource object.
-     *
-     * @param int|string|null $id
-     *
-     * @return ResponseInterface
-     */
-    public function edit($id = null)
-    {
-        //
-    }
-
-    /**
-     * Add or update a model resource, from "posted" properties.
-     *
-     * @param int|string|null $id
-     *
-     * @return ResponseInterface
-     */
-    public function update($id = null)
-    {
-        //
-    }
-
-    /**
-     * Delete the designated resource object from the model.
-     *
-     * @param int|string|null $id
-     *
-     * @return ResponseInterface
-     */
-    public function delete($id = null)
-    {
-        //
-    }
+    // methods lainnya
 }
